a0d8cdfcc794d9a953547f9947a7e4e1adf7e2f3
andrewmen <andrewmen@12e484f7-5e12-421d-bbfb-6194dbfb34fd>
2009-07-15 04:09:52 +0000

    unnecessary test
    git-svn-id: https://itrust.svn.sourceforge.net/svnroot/itrust@37 12e484f7-5e12-421d-bbfb-6194dbfb34fd

--- iTrust/unittests/edu/ncsu/csc/itrust/action/ViewVisitedHCPsActionTest.java
+++ iTrust/unittests/edu/ncsu/csc/itrust/action/ViewVisitedHCPsActionTest.java
@@ -1,14 +1,12 @@
 package edu.ncsu.csc.itrust.action;
 
+import java.util.List;
+import junit.framework.TestCase;
+import edu.ncsu.csc.itrust.beans.HCPVisitBean;
+import edu.ncsu.csc.itrust.beans.PersonnelBean;
 import edu.ncsu.csc.itrust.dao.DAOFactory;
 import edu.ncsu.csc.itrust.datagenerators.TestDataGenerator;
 import edu.ncsu.csc.itrust.testutils.TestDAOFactory;
-import edu.ncsu.csc.itrust.action.ViewVisitedHCPsAction;
-import edu.ncsu.csc.itrust.beans.HCPVisitBean;
-import edu.ncsu.csc.itrust.beans.PersonnelBean;
-import edu.ncsu.csc.itrust.exception.iTrustException;
-import java.util.List;
-import junit.framework.TestCase;
 
 public class ViewVisitedHCPsActionTest extends TestCase {
 
@@ -28,54 +26,28 @@ public class ViewVisitedHCPsActionTest extends TestCase {
 
 	public void testGetVisitedHCPsRed() {
 		List<HCPVisitBean> beans = action.getVisitedHCPs();
-		
 
 		assertEquals("Jason Frankenstein", beans.get(0).getHCPName());
 		assertEquals("Kelly Doctor", beans.get(1).getHCPName());
 		assertEquals("Gandalf Stormcrow", beans.get(2).getHCPName());
 	}
 
-	public void testGetVisitedHCPsRed2(){
-	List<HCPVisitBean> beans = action2.getVisitedHCPs();
+	public void testGetVisitedHCPsRed2() {
+		List<HCPVisitBean> beans = action2.getVisitedHCPs();
 
 		assertEquals("Kelly Doctor", beans.get(0).getHCPName());
 		assertEquals("Gandalf Stormcrow", beans.get(1).getHCPName());
 	}
 
-	public void testDeclareHCP() {
-		
-		try {
-			action.declareHCP("Kelly Doctor");
-		}
-		catch (iTrustException ie) {
-			fail("should have been successfull");
-		}
-		
-		/*try {
-			action.declareHCP("Kelly Doctor");
-			fail("should have thrown exeception");
-		}
-		catch (iTrustException ie) {
-			System.out.println(ie.getMessage());
-		}
-		*/
-		try {
-			action.undeclareHCP("Kelly Doctor");
-		}
-		catch (iTrustException ie) {
-			fail("should have been successfull");
-		}
-	}
-	
 	public void testCheckDeclared() {
 		assertEquals(false, action.checkDeclared(0));
 		assertEquals(true, action.checkDeclared(9000000003L));
 	}
-	
+
 	public void testFilterHCPList() {
 		List<PersonnelBean> beans = action.filterHCPList("Frank", "pediatrician", "");
-		
-		for (PersonnelBean bean: beans) {
+
+		for (PersonnelBean bean : beans) {
 			assertEquals("Lauren Frankenstein", bean.getFullName());
 		}
 	}
