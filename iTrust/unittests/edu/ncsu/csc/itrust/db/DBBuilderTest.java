595336473de281f287691d369734c9d06f56c4f1
andrewmen <andrewmen@12e484f7-5e12-421d-bbfb-6194dbfb34fd>
2009-07-15 04:20:33 +0000

    changed this to what it used to be... correct.
    git-svn-id: https://itrust.svn.sourceforge.net/svnroot/itrust@40 12e484f7-5e12-421d-bbfb-6194dbfb34fd

--- iTrust/unittests/edu/ncsu/csc/itrust/db/DBBuilderTest.java
+++ iTrust/unittests/edu/ncsu/csc/itrust/db/DBBuilderTest.java
@@ -2,15 +2,13 @@ package edu.ncsu.csc.itrust.db;
 
 import junit.framework.TestCase;
 import edu.ncsu.csc.itrust.DBBuilder;
-import edu.ncsu.csc.itrust.datagenerators.TestDataGenerator;
 
 public class DBBuilderTest extends TestCase {
 
 	// Make sure that the actual database can be rebuilt
 	// This is run twice so that we check the "drop tables" script
 	public void testRebuildNoException() throws Exception {
-		DBBuilder.main(new String[]{});
-		TestDataGenerator.main(new String[]{});
-		DBBuilder.main(new String[]{});
+		DBBuilder.rebuildAll();
+		DBBuilder.rebuildAll();
 	}
 }
