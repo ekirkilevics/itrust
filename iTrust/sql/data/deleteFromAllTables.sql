9e722f2f246af2e8ab18753e8e7ee6c12e3d20a2
andrewmen <andrewmen@12e484f7-5e12-421d-bbfb-6194dbfb34fd>
2009-08-12 15:57:55 +0000

    Very important that these are DELETE FROM and not TRUNCATE. TRUNCATE wipes out the AUTO_INCREMENT start value, which for tables like Personnel needs to start with 9 billion. This messes up our unit tests and auto_increment stuff.
    And TRUNCATE is actually slower. Even though it shouldn't be.
    
    git-svn-id: https://itrust.svn.sourceforge.net/svnroot/itrust@73 12e484f7-5e12-421d-bbfb-6194dbfb34fd

--- iTrust/sql/data/deleteFromAllTables.sql
+++ iTrust/sql/data/deleteFromAllTables.sql
@@ -1,35 +1,35 @@
-TRUNCATE DeclaredHCP;
-TRUNCATE FakeEmail;
-TRUNCATE GlobalVariables;
-TRUNCATE HCPAssignedHos;
-TRUNCATE HCPRelations;
-TRUNCATE LabProcedure;
-TRUNCATE LoginFailures;
-TRUNCATE LOINC;
-TRUNCATE Message;
-TRUNCATE referrals;
-TRUNCATE OVDiagnosis;
-TRUNCATE OVMedication;
-TRUNCATE OVProcedure;
-TRUNCATE OVSurvey;
-TRUNCATE OfficeVisits;
-TRUNCATE ReportRequests;
-TRUNCATE Representatives;
-TRUNCATE ResetPasswordFailures;
-TRUNCATE TransactionLog;
+DELETE FROM DeclaredHCP;
+DELETE FROM FakeEmail;
+DELETE FROM GlobalVariables;
+DELETE FROM HCPAssignedHos;
+DELETE FROM HCPRelations;
+DELETE FROM LabProcedure;
+DELETE FROM LoginFailures;
+DELETE FROM LOINC;
+DELETE FROM Message;
+DELETE FROM referrals;
+DELETE FROM OVDiagnosis;
+DELETE FROM OVMedication;
+DELETE FROM OVProcedure;
+DELETE FROM OVSurvey;
+DELETE FROM OfficeVisits;
+DELETE FROM ReportRequests;
+DELETE FROM Representatives;
+DELETE FROM ResetPasswordFailures;
+DELETE FROM TransactionLog;
 
 
-TRUNCATE PersonalAllergies;
-TRUNCATE PersonalHealthInformation;
-TRUNCATE PersonalRelations;
-TRUNCATE Allergies;
-TRUNCATE ICDCodes;
+DELETE FROM PersonalAllergies;
+DELETE FROM PersonalHealthInformation;
+DELETE FROM PersonalRelations;
+DELETE FROM Allergies;
+DELETE FROM ICDCodes;
 
 
-TRUNCATE Personnel;
-TRUNCATE Hospitals;
-TRUNCATE NDCodes;
+DELETE FROM Personnel;
+DELETE FROM Hospitals;
+DELETE FROM NDCodes;
 
-TRUNCATE CPTCodes;
-TRUNCATE Patients;
-TRUNCATE Users;
+DELETE FROM CPTCodes;
+DELETE FROM Patients;
+DELETE FROM Users;
