689d79e016a11b620f81c66b2321cd64f4c30da1
andrewmen <andrewmen@12e484f7-5e12-421d-bbfb-6194dbfb34fd>
2011-01-04 18:18:20 +0000

    still fixing mistakes...
    git-svn-id: https://itrust.svn.sourceforge.net/svnroot/itrust@152 12e484f7-5e12-421d-bbfb-6194dbfb34fd

--- iTrust/unittests/edu/ncsu/csc/itrust/dao/photo/ProfilePhotoDAOExceptionTest.java
+++ iTrust/unittests/edu/ncsu/csc/itrust/dao/photo/ProfilePhotoDAOExceptionTest.java
@@ -1,9 +1,8 @@
 package edu.ncsu.csc.itrust.dao.photo;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import junit.framework.TestCase;
-import edu.ncsu.csc.itrust.exception.iTrustException;
+import edu.ncsu.csc.itrust.exception.DBException;
 import edu.ncsu.csc.itrust.testutils.EvilDAOFactory;
 
 public class ProfilePhotoDAOExceptionTest extends TestCase {
@@ -12,8 +11,8 @@ public class ProfilePhotoDAOExceptionTest extends TestCase {
 		try {
 			EvilDAOFactory.getEvilInstance().getProfilePhotoDAO().get(1l);
 			fail("Exception should have been thrown");
-		} catch (iTrustException e) {
-			assertEquals(EvilDAOFactory.MESSAGE, e.getMessage());
+		} catch (DBException e) {
+			assertEquals(EvilDAOFactory.MESSAGE, e.getSQLException().getMessage());
 		}
 	}
 
@@ -22,8 +21,8 @@ public class ProfilePhotoDAOExceptionTest extends TestCase {
 			BufferedImage bi = new BufferedImage(900, 500, BufferedImage.TYPE_3BYTE_BGR);
 			EvilDAOFactory.getEvilInstance().getProfilePhotoDAO().store(1l, bi);
 			fail("Exception should have been thrown");
-		} catch (iTrustException e) {
-			assertEquals(EvilDAOFactory.MESSAGE, e.getMessage());
+		} catch (DBException e) {
+			assertEquals(EvilDAOFactory.MESSAGE, e.getSQLException().getMessage());
 		}
 	}
 }
