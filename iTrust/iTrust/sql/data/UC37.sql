2ebc4044561515f00a86e8b8fecf46bc15947a0f
jasontking <jasontking@12e484f7-5e12-421d-bbfb-6194dbfb34fd>
2012-08-15 00:55:53 +0000

    git-svn-id: https://itrust.svn.sourceforge.net/svnroot/itrust@217 12e484f7-5e12-421d-bbfb-6194dbfb34fd

--- iTrust/iTrust/sql/data/UC37.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Allergies(
-	PatientID,
-	Description,
-	Code,
-	FirstFound,
-)
-VALUES (25,'Penicillin','664662530','2012-2-3');
--- iTrust/iTrust/sql/data/ndcodes100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO NDCodes(Code, Description) VALUES
-('483013420','Midichlomaxene'),
-('483012382','Midichlominene')
-ON DUPLICATE KEY UPDATE Code = Code;
